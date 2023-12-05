2400f3d855cd39cfff95f3a2d88a90a4961c5092
pianndi <demakalviandi@gmail.com>
2023-12-05 14:44:56 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*"],
+  theme: {
+    screens: {
+      'sm': '567px'
+    },
+    extend: {},
+    colors: {
+      accent: "#3a10e5",
+      primary: "#fff",
+      secondary: "#f0f3ff",
+      ternary: "#facb3b",
+      dark: "#090e23",
+      grayy: "#9c9c9c"
+    }
+  },
+  plugins: [],
+}
+
+  
